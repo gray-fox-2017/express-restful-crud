405b3003a25363659291b604d9ae2ee998f71156
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-05-16 00:25:02 +0700

    still on progress

---
+++ migrations/20170515075045-create-mytodos.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('mytodos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      is_complete: {
+        type: Sequelize.STRING
+      },
+      user_mail: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('mytodos');
+  }
+};
