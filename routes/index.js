5d06c9f5cf7c56654db797a5c778329894641dbf
Sidik Hidayatullah <sidik.riders@gmail.com>
2017-05-16 21:39:05 +0700

    sepertinya sudah restfull

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,19 @@ router.get('/', function(req, res, next) {
   db.User.findAll().then(data => {
     res.render('index', {data:data});
   })
-
 });
+
+router.post('/', function(req, res, next) {
+  let aidi = req.body.aidi
+  db.User.findOne({
+    where: {
+      id: aidi
+    }
+  }).then( data => {
+    let input = `?id=${data.id}&user=${data.username}`
+    res.redirect(`/user/${input}`)
+  })
+})
 
 router.get('/all', function(req, res, next) {
   let todos = []
@@ -23,9 +34,57 @@ router.get('/all', function(req, res, next) {
   })
 })
 
-// router.post('/', (req, res, next) => {
-//   let username = req.body.username;
-//   res.redirect('/user', {username})
-// })
+router.get('/all/todo-delete/:id', function(req, res, next) {
+  let todo_id = req.params.id;
+
+  db.Todo.destroy({
+    where: {
+      id: todo_id
+    }
+  }).then(asal => {
+    res.redirect('/all')
+  })
+})
+
+router.get('/all/todo/:id', function(req, res, next) {
+  let todo_id = req.params.id;
+
+  db.Todo.update({
+    is_complete: true
+  },{
+      where: {
+      id: todo_id
+    }
+  }).then(asal => {
+    res.redirect('/all')
+  })
+})
+
+router.get('/all/todo-edit/:id', function(req, res, next) {
+  let todo_id = req.params.id;
+  db.Todo.findOne({
+    where: {
+      id: todo_id
+    }
+  }).then( data => {
+    res.render('editor-all', {data:data})
+  })
+})
+
+router.post('/all/update/:id', function(req, res, next) {
+  let todo_id = req.params.id;
+  let newTask = req.body.taskBaru;
+  db.Todo.update({
+    is_complete: false,
+    task: newTask,
+    updatedAt: new Date()
+  },{
+    where: {
+      id: todo_id
+    }
+  }).then( asal => {
+    res.redirect('/all')
+  })
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,24 +5,48 @@ const helper = require('../helper/util.js')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  let aidi = req.body.aidi;
+  let aidi = req.query.id
+  let username = req.query.user
   db.Todo.findAll({
     where: {
       user_id: aidi
     }
   }).then( data => {
-    res.render('user', {data:data, id:aidi, helper:helper})
+    res.render('user', {data:data, user:username, id:aidi, helper:helper})
   })
 })
 
-router.get('/todo-delete/:id', function(req, res, next) {
+router.post('/buat-baru', function(req, res, next) {
+  let aidi = req.body.aidi;
+  let newTask = req.body.taskBaru;
+  let username = '\(\∩\｀\-\´\)\⊃\━\☆\ﾟ\.\*\･\｡\ﾟ'
+  db.Todo.create({
+    task: newTask,
+    user_id:aidi,
+    is_complete: false,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  })
+  .then(asd => {
+    res.redirect(`/user/?id=${aidi}&user=${username}`)
+  })
+})
+
+router.get('/todo-delete/:user_id/:id', function(req, res, next) {
   let todo_id = req.params.id
+  let user_id = req.params.user_id
   db.Todo.destroy({
     where: {
       id: todo_id
     }
   }).then( test => {
-    res.redirect('/')
+    db.User.findOne({
+      where: {
+        id: user_id
+      }
+    }).then (data => {
+      res.redirect(`/user/?id=${data.id}&user=${data.username}`)
+    })
   })
 })
 
@@ -31,13 +55,26 @@ router.post('/update/:id', function(req, res, next) {
   let taskBaru = req.body.taskBaru
   db.Todo.update({
     task: taskBaru,
-    updatedAt: new Date()
+    updatedAt: new Date(),
+    is_complete: false
   }, {
     where: {
       id: todo_id
     }
   }).then((afsa) => {
-    res.redirect('/')
+    db.Todo.findOne({
+      where: {
+        id: todo_id
+      }
+    }).then(data => {
+      db.User.findOne({
+        where:{
+          id: data.user_id
+        }
+      }).then( data => {
+        res.redirect(`/user/?id=${data.id}&user=${data.username}`)
+      })
+    })
   })
 })
 
@@ -52,8 +89,9 @@ router.get('/todo-edit/:id', function(req, res, next) {
   })
 })
 
-router.get('/todo/:id', function(req, res, next) {
-  let todo_id = req.params.id
+router.get('/todo/:user_id/:id', function(req, res, next) {
+  let todo_id = req.params.id;
+  let user_id = req.params.user_id;
   db.Todo.update({
     is_complete: true,
     updatedAt: new Date()
@@ -61,39 +99,17 @@ router.get('/todo/:id', function(req, res, next) {
       id: todo_id
     }
   }).then( test => {
-    res.redirect('/')
+    db.User.findOne({
+      where: {
+        id: user_id
+      }
+    }).then( data => {
+      let username = data.username;
+      res.redirect(`/user/?id=${user_id}&user=${username}`)
+    })
   })
 })
 
-router.post('/', function(req, res, next) {
-  let aidi = req.body.aidi;
-  let newTask = req.body.taskBaru;
-  let imel = '';
-  let yuser = '';
-  if (newTask !== undefined && newTask !== null && newTask !== "") {
-    db.Todo.create({
-      task: newTask,
-      user_id: aidi,
-      is_complete: false,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }).then(() => {
-      res.redirect('/')
-    })
-  } else {
-    db.User.findById(aidi)
-    .then( data => {
-      imel = data.email;
-      yuser = data.username;
-    })
-    db.Todo.findAll({
-      where: {
-        user_id: aidi
-      }
-    }).then( data => {
-      res.render('user', {data:data, id:aidi, helper:helper, email:imel, user:yuser})
-    })
-  }
- });
+
 
 module.exports = router;
